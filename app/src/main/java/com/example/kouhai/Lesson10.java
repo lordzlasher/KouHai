e797d83fbebf32d16cb5381f506a74952c990760
(2005551040) Putu Yoga Pramana Putra <48519837+lordzlasher@users.noreply.github.com>
2022-06-06 03:34:23 +0800

    Lesson 6-10

---
+++ app/src/main/java/com/example/kouhai/Lesson10.java
@@ -0,0 +1,14 @@
+package com.example.kouhai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lesson10 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson10);
+    }
+}
