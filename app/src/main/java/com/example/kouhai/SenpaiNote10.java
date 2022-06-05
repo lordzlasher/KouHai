e91a5490820794ff433c84b05f8aa701f1c4bdfa
indah permata <indahpermatakadek83@gmail.com>
2022-06-06 01:26:27 +0800

    Senpai Note

---
+++ app/src/main/java/com/example/kouhai/SenpaiNote10.java
@@ -0,0 +1,14 @@
+package com.example.kouhai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SenpaiNote10 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_senpai_note10);
+    }
+}
